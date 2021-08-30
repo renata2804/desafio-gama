e743dee0ddb291c4807d92da5f3fe0f71ef0cd50
Renata <88511310+renata2804@users.noreply.github.com>
2021-08-29 22:16:51 -0300

    Error

--- desafioGama/rotas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const CandidateController = require('./candidateController');
-const routes = new express.Router();
-
-routes.post('/register', CandidateController.register);
-routes.get('/', (req, res) => {
-    res.send('Sucesso!!!');
-});
-module.exports = routes;
